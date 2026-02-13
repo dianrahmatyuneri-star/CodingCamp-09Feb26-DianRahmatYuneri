92b0b3aca41f5721335aa0388347e931ebe6bd8b
dianrahmatyuneri-star <dianrahmatyuneri@gmail.com>
2026-02-13 21:10:04 +0800

    Menambahkan welcome speech js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
- 
+welcomeMessage();
+
+//Welcome Messsage Function
+function welcomeMessage(){
+    //Show Pop Up
+    let name = prompt("Welcome to Dian Rahmat Yuneri Company! What is your name?");
+
+    //Validation Input
+    if (name===null || name.trim()==="") {
+        //Jika user pencet cancel, maka jadi "Guest"
+        name="Guest";
+    }
+
+    //Display Welcome Message
+    document.getElementById('welcome-speech').innerHTML = `Hello, ${name}! Welcome to Dian Rahmat Yuneri Company!`;
+}
