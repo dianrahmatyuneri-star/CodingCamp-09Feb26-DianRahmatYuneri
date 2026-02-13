a62070107b9650fe4a4980e3bb53db72e27beb65
dianrahmatyuneri-star <dianrahmatyuneri@gmail.com>
2026-02-13 22:02:35 +0800

    Telah menyelesaikan form validasi ketika input message

--- js/script.js
+++ js/script.js
@@ -14,3 +14,48 @@ function welcomeMessage(){
     //Display Welcome Message
     document.getElementById('welcome-speech').innerHTML = `Hello, ${name}! Welcome to Dian Rahmat Yuneri Company!`;
 }
+
+const form = document.getElementById("messageForm");
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); // stop kirim form dulu
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    let valid = true;
+
+    // Reset pesan error
+    document.getElementById("nameError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("messageError").textContent = "";
+
+    //Validasi nama
+    if (name === "") {
+        document.getElementById("nameError").textContent = "Name is required";
+        valid = false;
+    }
+
+    // Validasi email (regex)
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email === "") {
+        document.getElementById("emailError").textContent = "Email cannot be empty";
+        valid = false;
+    }
+    else if (!emailPattern.test(email)) {
+        document.getElementById("emailError").textContent = "Email is not valid";
+        valid = false;
+    }
+
+    //Validasi pesan
+    if (message === "") {
+        document.getElementById("messageError").textContent = "Message cannot be empty";
+        valid = false;
+    }
+
+    //Jike semua benar
+    if (valid) {
+        alert("Your message has been sent successfully!");
+    }
+});
